4e4be1bb43c499196af123d9f5d3b46dfdecd229
mrlinguinicode <mrlinguini@protonmail.com>
2022-02-22 19:33:46 -0500

    register modal

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { flexbox } from "@mui/system";
 function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
   const [openModal, setOpenModal] = useState(false);
 
   const style = {
@@ -28,50 +29,96 @@ function App() {
   const handlePassword = (e) => {
     setPassword(e.target.value);
   };
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
   const handleOpen = () => setOpenModal(true);
   const handleClose = () => setOpenModal(false);
 
   return (
-    <div className="login-container">
+    <>
       <nav>
         <h1>Task Manager</h1>
       </nav>
-      <div className="login-items">
-        <TextField
-          id="outlined-basic"
-          label="Email"
-          variant="outlined"
-          onChange={handleEmail}
-        />
-        <span className="span"></span>
-        <TextField
-          id="outlined-basic"
-          label="Password"
-          variant="outlined"
-          onChange={handlePassword}
-        />
-        <span className="span"></span>
-        <div className="login-buttons">
-          <Button
-            variant="contained"
-            onClick={() => {
-              console.log(email);
-              console.log(password);
-            }}
-          >
-            Login
-          </Button>
-          <Button variant="contained" onClick={() => handleOpen()}>
-            Register
-          </Button>
-          <Modal classNmae="modal" open={openModal} onClose={handleClose}>
-            <Box className="modal-box">
-              <p>whaddip</p>
-            </Box>
-          </Modal>
+      <div className="login-container">
+        <div className="login-items">
+          <TextField
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            onChange={handleEmail}
+          />
+          <span className="span"></span>
+          <TextField
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            type="password"
+            onChange={handlePassword}
+          />
+          <span className="span"></span>
+          <div className="login-buttons">
+            <Button
+              variant="contained"
+              onClick={() => {
+                console.log(email);
+                console.log(password);
+              }}
+            >
+              Login
+            </Button>
+            <Button variant="contained" onClick={() => handleOpen()}>
+              Register
+            </Button>
+            <Modal className="modal" open={openModal} onClose={handleClose}>
+              <Box>
+                <form className="modal-box">
+                  <div className="register-items">
+                    <TextField
+                      id="outlined-basic"
+                      label="Name"
+                      variant="outlined"
+                      onChange={handleName}
+                    />
+                  </div>
+                  <div className="register-items">
+                    <TextField
+                      id="outlined-basic"
+                      label="Email"
+                      variant="outlined"
+                      onChange={handleEmail}
+                    />
+                  </div>
+                  <div className="register-items">
+                    <TextField
+                      id="outlined-basic"
+                      label="Password"
+                      variant="outlined"
+                      type="password"
+                      onChange={handlePassword}
+                    />
+                  </div>
+                  <div className="login-buttons">
+                    <Button
+                      variant="contained"
+                      onClick={() => {
+                        handleClose();
+                        console.log(name);
+                        console.log(email);
+                        console.log(password);
+                      }}
+                    >
+                      Register
+                    </Button>
+                  </div>
+                </form>
+              </Box>
+            </Modal>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
